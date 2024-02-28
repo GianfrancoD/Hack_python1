fed52aade9531c7aa9f7494a5a23643d5063b235
GianfrancoD <gianfranco.dfiore@gmail.com>
2024-02-28 00:06:30 -0400

    HACK PYTHON 1

--- hack_7.py
+++ hack_7.py
@@ -4,11 +4,11 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    result = range(0,5+1)
-    number_result = []
 
-    for i in result[::-1]:
-        number_result.append(i)
-    return number_result
+    i = 5
+    while i >= 0:
+        result.append(i)
+        i -= 1
 
+    return result
 print(fn_hack_7())
